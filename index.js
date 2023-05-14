b1cc3faa15bcf3266a487af275bc949784102f10
Gabriel Torres <gabrieltlpires@gmail.com>
2023-05-14 12:51:18 -0300

    Corrige lógica e adiciona novas marcações

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+function calcular(){
 var gramaCarneCrianca = 200
 var gramaCarneAdulto = 400
 var gramaAcompanhamentosAdulto = 200
@@ -9,12 +10,22 @@ var refrigeranteMlCrianca = 500
 var aguaMlAdulto = 400
 var aguaMlcrianca = 400
 
-window.alert('----- CHURRASCO -----')
-window.alert('Quantidade de itens necessários para um churrasco')
+var AdultoSemAlcool = window.document.getElementById('txtad')
+var AdultoAlcool = window.document.getElementById('txtadalc')
+var Crianca = window.document.getElementById('txtcrian')
 
-var qtdAdulto = Number(window.prompt('Quantidade de Adultos (que não consomem bebidas alcoólicas):'))
-var qtdAdultoAlcool = Number(window.prompt('Quantidade de Adultos (que consomem bebidas alcoólicas)'))
-var qtdcriancas = Number(window.prompt('Quantidade de Crianças: '))
+qtdAdulto = Number(AdultoSemAlcool.value)
+qtdAdultoAlcool = Number(AdultoAlcool.value)
+qtdcriancas = Number(Crianca.value)
+if (qtdAdulto < 0 || qtdAdultoAlcool < 0 || qtdcriancas < 0){
+    document.getElementById("resposta").innerHTML = `<p>Quantidade de pessoas <strong>INVÁLIDA!</strong></p>`
+    document.getElementById("resposta").innerHTML += `<p>Por favor insira um valor maior que <strong>ZERO</strong>.</p>`
+    document.getElementById("carne").innerText = "INVÁLIDO"
+    document.getElementById("acompanhamento").innerText = "INVÁLIDO"
+    document.getElementById("cerveja").innerText = "INVÁLIDO"
+    document.getElementById("refrigerante").innerText = "INVÁLIDO"
+    document.getElementById("agua").innerText = "INVÁLIDO"
+}else{
 
 var resultadoCarne = (qtdAdulto * gramaCarneAdulto + qtdAdultoAlcool * gramaCarneAdulto + qtdcriancas * gramaCarneCrianca) / 1000
 var resultadoAcompanhamentos = (qtdAdulto * gramaAcompanhamentosAdulto + qtdAdultoAlcool * gramaAcompanhamentosAdulto + qtdcriancas * gramaAcompanhamentosCrianca) / 1000  
@@ -22,17 +33,11 @@ var resultadoCerveja = (qtdAdultoAlcool * cervejaLitroAdulto)
 var resultadoRefrigerante = (qtdAdulto * refrigeranteMlAdulto + qtdAdultoAlcool * refrigeranteMlAdulto + qtdcriancas * refrigeranteMlCrianca) / 1000
 var resultadoAgua = (qtdAdulto * aguaMlAdulto + qtdAdultoAlcool * aguaMlAdulto + qtdcriancas * aguaMlcrianca) / 1000
 
-document.getElementById("adultos").innerText = `Adultos (que consomem bebidas alcoólicas).......: ${qtdAdultoAlcool}`
-document.getElementById("adultosAlcool").innerText = `Adultos (que não consomem bebidas alcoólicas)...: ${qtdAdulto}`
-document.getElementById("criancas").innerText = `Crianças........................................: ${qtdcriancas}`
-document.getElementById("carne").innerText = `Carne.............: ${resultadoCarne}kg`
-document.getElementById("acompanhamento").innerText = `Acompanhamentos...: ${resultadoAcompanhamentos}kg`
-document.getElementById("cerveja").innerText = `Cerveja...........:${resultadoCerveja}L`
-document.getElementById("refrigerante").innerText = `Refrigerante......: ${resultadoRefrigerante}L`
-document.getElementById("agua").innerText = `Água..............: ${resultadoAgua}L`
-
-
-
-
-
-
+document.getElementById("resposta").innerHTML = `<p>Resultado:</p>`
+document.getElementById("carne").innerHTML = `${resultadoCarne}kg`
+document.getElementById("acompanhamento").innerText = `${resultadoAcompanhamentos}kg`
+document.getElementById("cerveja").innerText = `${resultadoCerveja}L`
+document.getElementById("refrigerante").innerText = `${resultadoRefrigerante}L`
+document.getElementById("agua").innerText = `${resultadoAgua}L`
+}
+}
